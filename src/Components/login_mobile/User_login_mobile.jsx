34295147b84aed6b6923f2a433cb56bb25de25c0
Rujapa215 <rujapa.dh@gmail.com>
2023-09-18 21:18:37 +0700

    add more pages

--- src/Components/login_mobile/User_login_mobile.jsx
+++ src/Components/login_mobile/User_login_mobile.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import user_login_mobile_pic from '../../assets/pic_login_mobile/user_login_mobile_pic.svg'
-// import AiOutlineArrowLeft "react-icons/ai";
+import {AiOutlineArrowLeft} from "react-icons/ai";
+
 
  const User_login_mobile = () => {
   return (
     <div class="grid justify-center sm:max md:disabled: gap-4" >
-      <div>
+      <div className='relative'>
       <img src={user_login_mobile_pic} className='h-[355px] w-100' alt="" />
+      <AiOutlineArrowLeft  className='absolute inline-block  top-0 left-0 bg-white'/>
       </div>
        <div  className='mx-[auto]'>
        <p className='text-[36px]'>Welcome back!</p>
@@ -16,12 +18,11 @@ import user_login_mobile_pic from '../../assets/pic_login_mobile/user_login_mobi
        <p className='text-[36px]'>Log in</p>
        </div>
        <div>
-        <input type="text" name="" id="" placeholder='  user name' className='border-2 border-slate-900 w-full  rounded-lg'/>
+        <input type="text" name="" id="" placeholder='  user name' className='h-[50px] border-2 border-slate-900 w-full  rounded-lg'/>
        </div>
        <div >
-        <input type="text" name="" id="" placeholder='  password' className='border-2 border-slate-900 w-full  rounded-lg'/>
+        <input type="text" name="" id="" placeholder='  password' className='h-[50px] border-2 border-slate-900 w-full  rounded-lg'/>
        </div>
-       <br />
        <button  class="bg-myGreen  text-black  py-2 px-4 rounded self-center" >Log in</button>
        <div className='mx-[auto]'>
         <a href=""> forgot password?</a>
