e8a11895620380174cf6217107c9917efd1500ad
timworanon <139347732+timworanon@users.noreply.github.com>
2023-09-26 21:54:05 +0700

    Edit-Homepage

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,16 +2,14 @@ import Hero from "../../components/Hero/Hero"
 import Discription from "../../Components/Hero/Discription"
 import HomeSignup from "../../Components/Hero/HomeSignup"
 import Activity from "../../Components/Avtivities/Activity";
-import DiaryCard from "../../Components/Diary/DiaryCard";
 
 function HomePage() {
   return (
     <>
-    {/* <Hero/>
+    <Hero/>
     <Discription/>
     <HomeSignup/>
-    <Activity /> */}
-    <DiaryCard />
+    <Activity />
     </>
   )
 }
