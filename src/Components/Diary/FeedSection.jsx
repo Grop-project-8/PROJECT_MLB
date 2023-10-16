9134adf41a9abbcc0afaefc679de47bcc569dddb
timworanon <139347732+timworanon@users.noreply.github.com>
2023-10-16 21:41:26 +0700

    Edit FeedSection

--- src/Components/Diary/FeedSection.jsx
+++ src/Components/Diary/FeedSection.jsx
@@ -4,9 +4,9 @@ const FeedSection = () => {
     return (
         <div>
             <div >
-            <div class='max-w-[600px] flex m-1'>
+            <div className='max-w-[600px] flex m-1'>
                 <img
-                class='rounded-full h-12 w-18 ' 
+                className='rounded-full h-12 w-18 ' 
                 src='https://img.freepik.com/premium-vector/young-smiling-man-avatar-man-with-brown-beard-mustache-hair-wearing-yellow-sweater-sweatshirt-3d-vector-people-character-illustration-cartoon-minimal-style_365941-860.jpg'
                 alt='User 1'
                 />
