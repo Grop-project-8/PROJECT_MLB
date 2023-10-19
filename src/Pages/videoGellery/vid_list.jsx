ad7e990df6666f010e944f4ca6d90f2dc504925b
Rujapa215 <rujapa.dh@gmail.com>
2023-10-19 21:56:33 +0700

    pond trying to push video gellery

---
+++ src/Pages/videoGellery/vid_list.jsx
@@ -0,0 +1,148 @@
+const vid_list = {
+    yoga:[{
+        youtube_embed_link: 'https://www.youtube.com/embed/I5bjG6uRWWg',
+        title: 'TONED ARMS WORKOUT - (quick + intense)',
+        equipment:'none',
+        tumbnail: 'https://i1.ytimg.com/vi/I5bjG6uRWWg/mqdefault.jpg'
+      }],
+    pilates:[{
+        youtube_embed_link: 'https://www.youtube.com/embed/I5bjG6uRWWg',
+        title: 'TONED ARMS WORKOUT - (quick + intense)',
+        equipment:'none',
+        tumbnail: 'https://i1.ytimg.com/vi/I5bjG6uRWWg/mqdefault.jpg'
+      }],
+    bodyWeight:[  {
+        youtube_embed_link: 'https://www.youtube.com/embed/I5bjG6uRWWg',
+        title: 'TONED ARMS WORKOUT - (quick + intense)',
+        equipment:'none',
+        tumbnail: 'https://i1.ytimg.com/vi/I5bjG6uRWWg/mqdefault.jpg'
+      },
+    //ตัว
+      {
+        youtube_embed_link: 'https://www.youtube.com/embed/jKTxe236-4U',
+        title: '30 MIN FULL BODY WORKOUT - Apartment &amp; Small Space Friendly (No Equipment, No Jumping)',
+        equipment:'none',
+        tumbnail_: 'https://i1.ytimg.com/vi/jKTxe236-4U/mqdefault.jpg'
+      },
+    //ขา&สะโพก
+     {
+        youtube_embed_link: 'https://www.youtube.com/embed/9hQTvrP6EsM',
+        title: 'TONED LEGS &amp; ROUND BOOTY At Home Workout (No Equipment)',
+        equipment:'Rubber band',
+        tumbnail_: 'https://i1.ytimg.com/vi/9hQTvrP6EsM/mqdefault.jpg'
+      },
+    //Back & CORE
+     {
+        youtube_embed_link: 'https://www.youtube.com/embed/aPF7rUv0CZs',
+        title: '15 MIN TONED BACK &amp; CORE - No Equipment, No Repeat Exercises, Low Impact Home Workout',
+        equipment:'none',
+        tumbnail_: 'https://i1.ytimg.com/vi/aPF7rUv0CZs/mqdefault.jpg'
+      },
+    //หน้าท้อง
+     {
+        youtube_embed_link: 'https://www.youtube.com/embed/svdPTfOpAyQ',
+        title: '10 MIN SIX PACK ABS for TOTAL BEGINNERS (No Equipment)',
+        equipment:'none',
+        tumbnail_: 'https://i1.ytimg.com/vi/jKTxe236-4U/mqdefault.jpg'
+      },
+    ],
+    dance:[{
+        youtube_embed_link: "https://www.youtube.com/embed/1vRto-2MMZo",
+        title: '"15 MIN DANCE PARTY WORKOUT ',
+        equipment:'none',
+        tumbnail_: 'https://i1.ytimg.com/vi/1vRto-2MMZo/mqdefault.jpg'
+      },
+      {
+        youtube_embed_link:"https://www.youtube.com/embed/U0u1-X8yZ48",
+        title: "14 Minute Seated Dance Workout To Improve Upper Body Coordination" ,
+        equipment:'none',
+        tumbnail_: 'https://i1.ytimg.com/vi/HBhVZpX4JZw/mqdefault.jpg'
+      },
+      {
+        youtube_embed_link: 'https://www.youtube.com/embed/jKTxe236-4U',
+        title: "10 Minute Leg Burner | HIIT Dance",
+        equipment:'none',
+        tumbnail_: 'https://i1.ytimg.com/vi/jKTxe236-4U/mqdefault.jpg'
+      },
+      {
+        youtube_embed_link:"https://www.youtube.com/embed/bre1FJR0ggo",
+        title: "20 Minute Intermediate Pop Music Dance Workout",
+        equipment:'none',
+        tumbnail_: 'https://i1.ytimg.com/vi/bre1FJR0ggo/mqdefault.jpg'
+      },{
+        youtube_embed_link: "https://www.youtube.com/embed/1shZt-ThNs4",
+        title: "BTS Full Body Dance Workout",
+        equipment:'none',
+        tumbnail_: 'https://i1.ytimg.com/vi/1shZt-ThNs4/mqdefault.jpg'
+      },
+      {
+        youtube_embed_link:"https://www.youtube.com/embed/RFfbZV3bwys",
+        title:"15 MIN HYPE DANCE WORKOUT",
+        equipment:'none',
+        tumbnail_: 'https://i1.ytimg.com/vi/RFfbZV3bwys/mqdefault.jpg'
+      },
+    ],
+    jumpingRope:[{
+        youtube_embed_link: 'https://www.youtube.com/embed/kDOGb9C5kp0',
+        title: 'How To Start Jumping Rope | Beginner Guide',
+        equipment:'rope',
+        tumbnail: 'https://i1.ytimg.com/vi/kDOGb9C5kp0/mqdefault.jpg'
+      },
+    {
+        youtube_embed_link: 'https://www.youtube.com/embed/1-KvIEU03yc',
+        title: 'Learn The Jump Rope Boxer Skip In 5 Easy Steps',
+        equipment:'rope',
+        tumbnail: 'https://i1.ytimg.com/vi/1-KvIEU03yc/mqdefault.jpg'
+      },
+    {
+        youtube_embed_link: 'https://www.youtube.com/embed/1BZM2Vre5oc',
+        title: '10 Min Beginner Jump Rope Workout',
+        equipment:'rope',
+        tumbnail: 'https://i1.ytimg.com/vi/1BZM2Vre5oc/mqdefault.jpg'
+      },
+    {
+        youtube_embed_link: 'https://www.youtube.com/embed/9qosVFBphcg',
+        title: 'How To Jump Rope Like A Boxer',
+        equipment:'rope',
+        tumbnail: 'https://i1.ytimg.com/vi/9qosVFBphcg/mqdefault.jpg'
+      },
+    {
+        youtube_embed_link: 'https://www.youtube.com/embed/T_tVMJEkUpg',
+        title: 'How to Jump Rope | Beginner Tutorial from Crossrope',
+        equipment:'rope',
+        tumbnail: 'https://i1.ytimg.com/vi/T_tVMJEkUpg/mqdefault.jpg'
+      },
+    {
+        youtube_embed_link: 'https://www.youtube.com/embed/y-SBA0aHZzw',
+        title: '15 Min HIIT Jump Rope Workout',
+        equipment:'rope',
+        tumbnail: 'https://i1.ytimg.com/vi/y-SBA0aHZzw/mqdefault.jpg'
+      },
+    {
+        youtube_embed_link: 'https://www.youtube.com/embed/TeThU_Xyl0c',
+        title: '600 Calorie Burn Jump Rope Workout',
+        equipment:'rope',
+        tumbnail: 'https://i1.ytimg.com/vi/TeThU_Xyl0c/mqdefault.jpg'
+      },
+     {
+        youtube_embed_link: 'https://www.youtube.com/embed/dRhlnvY01N4',
+        title: 'Only 15 Minutes To Burn 500 Calories!?',
+        equipment:'rope',
+        tumbnail: 'https://i1.ytimg.com/vi/dRhlnvY01N4/mqdefault.jpg'
+      },
+     {
+        youtube_embed_link: 'https://www.youtube.com/embed/ANR7qxarlNs',
+        title: '700 Calorie Burn Jump Rope Workout',
+        equipment:'rope',
+        tumbnail: 'https://i1.ytimg.com/vi/ANR7qxarlNs/mqdefault.jpg'
+      },
+    {
+        youtube_embed_link: 'https://www.youtube.com/embed/Sz96SdflS9A',
+        title: 'Jump Rope Fundamentals For Beginners',
+        equipment:'rope',
+        tumbnail: 'https://i1.ytimg.com/vi/Sz96SdflS9A/mqdefault.jpg'
+      }]
+
+    };
+
+export default vid_list;
